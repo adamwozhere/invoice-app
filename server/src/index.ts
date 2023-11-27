e2ef40eb816cdc675799106fce493042802e4484
Adam Wozniak <adamwozhere@gmail.com>
2023-11-27 22:44:06 +0000

    initial setup

---
+++ server/src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 3000;
+
+app.get('/ping', (_, res) => {
+  console.log('someone pinged here');
+  res.send('pong');
+});
+
+app.use(express.static('../client/dist'));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
