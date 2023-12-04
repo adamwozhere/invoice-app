0431d996a4ef853f90ec71d8672d2eb493926fd8
Adam Wozniak <adamwozhere@gmail.com>
2023-12-04 18:10:01 +0000

    fix: typescript errors in base client index page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,16 +9,19 @@ function App() {
   const [response, setResponse] = useState('');
 
   useEffect(() => {
-    axios.get('/ping').then((message) => setResponse(message.data));
+    axios
+      .get('/ping')
+      .then((message) => setResponse(message.data as string))
+      .catch((err) => console.log(err));
   });
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
