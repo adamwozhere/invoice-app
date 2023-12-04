9d6d99aea2942fd4917084cacbad4d22069de1e1
Adam Wozniak <adamwozhere@gmail.com>
2023-12-04 18:13:06 +0000

    add TEST_MONGODB_URI

--- server/src/utils/config.ts
+++ server/src/utils/config.ts
@@ -1,8 +1,21 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-const MONGODB_URI = process.env.MONGODB_URI || '';
-const PORT = process.env.PORT || '';
+if (process.env.MONGODB_URI === undefined) {
+  throw Error('Environment var not defined: MONGODB_URI');
+}
+
+if (process.env.TEST_MONGODB_URI === undefined) {
+  throw Error('Environment var not defined: TEST_MONGODB_URI');
+}
+
+const MONGODB_URI =
+  process.env.NODE_ENV === 'test'
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI;
+
+const PORT = process.env.PORT || 3000;
+
 const FRONTEND = '../client/dist';
 
 export default {
