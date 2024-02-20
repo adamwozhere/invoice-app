7a26ba2b9f0091c2ee18c0941feec8f1495f4701
Adam Wozniak <adamwozhere@gmail.com>
2024-02-20 18:49:53 +0000

    setup tanstack router and tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,46 +1,10 @@
-import { useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import axios from 'axios';
-
-function App() {
-  const [count, setCount] = useState(0);
-  const [response, setResponse] = useState('');
-
-  useEffect(() => {
-    axios
-      .get('/ping')
-      .then((message) => setResponse(message.data as string))
-      .catch((err) => console.log(err));
-  });
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <p>Server ping response: {response}</p>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1 className="text-4xl font-bold">Hello World!</h1>
+    </div>
   );
-}
+};
 
 export default App;
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createRouter } from '@tanstack/react-router';
+import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// import generated route tree
+import { routeTree } from './routeTree.gen';
+
+// create router instance
+const router = createRouter({ routeTree });
+
+// register router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+// render app
+const rootElement = document.getElementById('app')!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <App />
+      <RouterProvider router={router} />
+    </StrictMode>
+  );
+}
+
