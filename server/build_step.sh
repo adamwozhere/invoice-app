fd7176492999277aae33c2f2972ef627bcbdf102
Adam Wozniak <adamwozhere@gmail.com>
2024-05-10 14:44:30 +0100

    update build_step

--- server/build_step.sh
+++ server/build_step.sh
@@ -3,8 +3,13 @@
 echo "Build script"
 
 # add the commands here
+echo "Installing server dependencies"
 npm install
+npm run build
+
+echo "Installing client dependencies"
 cd ../client
 npm install
-cd ../server
-npm run build:full
+
+echo "Copying client/dist to server/dist"
+cp -r dist ../server/dist
