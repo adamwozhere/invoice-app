5b74ebe8d6759f20146269978f080730a35e98c9
Adam Wozniak <adamwozhere@gmail.com>
2024-05-10 15:04:29 +0100

    update build_step

--- server/build_step.sh
+++ server/build_step.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-echo "Build script"
+echo "Invoice App build script"
 
-# add the commands here
 echo "Installing server dependencies"
-npm install
-npm run build
+npm install && npm run build
 
 echo "Installing client dependencies"
-cd ../client
-npm install
-npm run build
+&& cd ../client && npm install && npm run build
 
 echo "Copying client/dist to server/dist"
-cp -r dist ../server/dist
+&& cp -r dist ../server/dist
