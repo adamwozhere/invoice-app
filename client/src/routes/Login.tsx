d5f9375ab85a155f59b5a81537b8f648b4ae9bd4
Adam Wozniak <adamwozhere@gmail.com>
2024-05-13 22:58:09 +0100

    fix: recreate Login.tsx to fix titlecase build error

---
+++ client/src/routes/Login.tsx
@@ -0,0 +1,9 @@
+import LoginForm from '../components/LoginForm';
+
+export default function Login() {
+  return (
+    <div className="max-w-sm w-full h-full flex flex-col justify-center">
+      <LoginForm />
+    </div>
+  );
+}
