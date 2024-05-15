dc68baf970a6e3b8380ae758f979e84363d469bb
Adam Wozniak <adamwozhere@gmail.com>
2024-05-15 19:49:24 +0100

    use cookie-parser for partitioned cookie after @types CookieOptions update

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -3,18 +3,18 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import logger from '../utils/logger';
 import User, { UserDocument } from '../models/user.model';
-import config from '../utils/config';
+import config from '../config';
 
-// TODO: proper config for getting the jwt secrets from env vars. keeping them typed etc, and the jwt.sub ? or use jwt.id ?
-// TODO: jwt / cookie options object for being able to change secure cookies on and off for dev/test or production
 // TODO: password validation in the user.model or auth.service, and put auth util functions in there ? or jwt utils in /utils ?
 
 export const loginHandler = async (
   req: Request<object, object, { email: string; password: string }>,
   res: Response
 ) => {
-  const { cookies } = req;
   const { email, password } = req.body;
+  const { cookies } = req;
+
+  const refreshCookie = cookies?.['__Host-jwt'];
 
   const user = await User.findOne<UserDocument>({ email });
 
@@ -30,12 +30,10 @@ export const loginHandler = async (
   const userData = {
     name: user.name,
     email: user.email,
-    // id: user.id,
     sub: user.id,
   };
 
   const accessToken = jwt.sign(userData, config.ACCESS_TOKEN_SECRET, {
-    // expiresIn: '30s',
     expiresIn: config.ACCESS_TOKEN_TTL,
   });
 
@@ -43,23 +41,22 @@ export const loginHandler = async (
     { ...userData, nonce: Math.random().toString().substring(7) },
     config.REFRESH_TOKEN_SECRET,
     {
-      // expiresIn: '5m',
       expiresIn: config.REFRESH_TOKEN_TTL,
     }
   );
 
   // get users refresh tokens array, and remove current token from cookie if cookie present
-  let newRefreshTokenArray = cookies?.jwt
-    ? user.refreshToken.filter((t) => t !== cookies.jwt)
+  let newRefreshTokenArray = refreshCookie
+    ? user.refreshToken.filter((t) => t !== refreshCookie)
     : user.refreshToken;
 
-  if (cookies?.jwt) {
+  if (refreshCookie) {
     // scenario:
     // 1) User logs in but never uses RT and does not logout
     // 2) RT is stolen
     // 3) If 1 and 2, reuse detection is needed to clear all RTs when user logs in
 
-    const refreshToken = cookies.jwt;
+    const refreshToken = refreshCookie;
     const foundToken = await User.findOne({ refreshToken });
 
     // detected refresh token reuse
@@ -79,14 +76,8 @@ export const loginHandler = async (
   const result = await user.save();
   logger.info(JSON.stringify(result));
 
-  // res.cookie('jwt', newRefreshToken, config.COOKIE_OPTIONS);
-  // TRY PARTITIONED COOKIE
-  res.setHeader(
-    'Set-Cookie',
-    `__Host-jwt=${newRefreshToken}; Max-Age=${config.COOKIE_OPTIONS.maxAge}; Path=/; HttpOnly; Secure; SameSite=None; Partitioned;`
-  );
+  res.cookie('__Host-jwt', newRefreshToken, config.COOKIE_OPTIONS);
 
-  // return res.json({ accessToken, email: user.email });
   return res.json({ accessToken });
 };
 
@@ -94,14 +85,15 @@ export const logoutHandler = async (req: Request, res: Response) => {
   // NOTE: Also need to delete bearer token on client
 
   const { cookies } = req;
+  const refreshCookie = cookies?.['__Host-jwt'];
 
   logger.info(`logoutHandler cookies are: ${JSON.stringify(cookies)}`);
 
-  if (!cookies?.['__Host-jwt']) {
+  if (!refreshCookie) {
     return res.sendStatus(204);
   }
 
-  const refreshToken = cookies['__Host-jwt'];
+  const refreshToken = refreshCookie;
   logger.info(`logout: refreshToken: ${JSON.stringify(refreshToken)}`);
 
   // is refresh token in db ?
@@ -118,13 +110,9 @@ export const logoutHandler = async (req: Request, res: Response) => {
   const result = await foundUser.save();
   logger.info(JSON.stringify(result));
 
-  // TODO: note that may need to set cors credentials whitelist / allowedOrigins for chrome
-  // also add sameSite: 'None', secure: true
-  // NOTE: secure: true does not work in thunderclient so have set to false currently,
-  // TODO: add check for production/dev - set cookie options secure: true if in production mode
+  // NOTE: cookie with secure: true may not work with thunderclient in local dev -
+  // Could add check for production/dev - set cookie options secure: true if in production mode
   // https://www.youtube.com/watch?v=favjC6EKFgw&list=PL0Zuz27SZ-6PFkIxaJ6Xx_X46avTM1aYw&index=16&ab_channel=DaveGray
-
-  // res.header('Access-Control-Allow-Credentials', 'true') ;???
 
   res.clearCookie('__Host-jwt');
 
@@ -133,17 +121,17 @@ export const logoutHandler = async (req: Request, res: Response) => {
 
 export const refreshHandler = async (req: Request, res: Response) => {
   const { cookies } = req;
+  const refreshCookie = cookies?.['__Host-jwt'];
 
   logger.info(`cookie: ${JSON.stringify(cookies)}`);
-  if (!cookies?.['__Host-jwt']) {
+  if (!refreshCookie) {
     return res.sendStatus(401);
   }
 
-  const refreshToken = cookies['__Host-jwt'];
+  const refreshToken = refreshCookie;
   logger.info(`refreshToken: ${refreshToken}`);
   res.clearCookie('__Host-jwt');
 
-  // try {
   const foundUser = await User.findOne<UserDocument>({ refreshToken }); // add .exec() at the end?
 
   if (!foundUser) {
@@ -156,19 +144,19 @@ export const refreshHandler = async (req: Request, res: Response) => {
     const userFromToken = await User.findOne({ _id: decoded.sub });
 
     userFromToken!.refreshToken = [];
-    const result = await userFromToken?.save();
+    const result = await userFromToken!.save();
 
     logger.info(JSON.stringify(result));
 
     return res.sendStatus(403); // Forbidden
   }
 
+  // remove old refresh token as it has now just been used
   const newRefreshTokenArray = foundUser.refreshToken.filter(
     (t) => t !== refreshToken
   );
 
   const decoded = jwt.verify(refreshToken, config.REFRESH_TOKEN_SECRET);
-  // should I be using jwt.sub here? - what do I actually need in the jwt? what can the front end actually use?
 
   // this should never happen?
   if (foundUser.id !== decoded.sub) {
@@ -177,11 +165,11 @@ export const refreshHandler = async (req: Request, res: Response) => {
 
   const userData = {
     name: foundUser.name,
-    email: foundUser.email, // tutorial just uses username
-    // id: foundUser.id,
+    email: foundUser.email,
     sub: foundUser.id,
   };
 
+  // create new access token
   const accessToken = jwt.sign(userData, config.ACCESS_TOKEN_SECRET, {
     expiresIn: config.ACCESS_TOKEN_TTL,
   });
@@ -200,23 +188,9 @@ export const refreshHandler = async (req: Request, res: Response) => {
   logger.info(JSON.stringify(result));
 
   // create new refreshToken cookie
-  // res.cookie('jwt', newRefreshToken, config.COOKIE_OPTIONS);
+  res.cookie('__Host-jwt', newRefreshToken, config.COOKIE_OPTIONS);
 
-  // TRY PARTITIONED COOKIE
-  res.setHeader(
-    'Set-Cookie',
-    `__Host-jwt=${newRefreshToken}; Max-Age=${config.COOKIE_OPTIONS.maxAge}; Path=/; HttpOnly; Secure; SameSite=None; Partitioned;`
-  );
-
-  // send back access token and any other data (could send the user details etc?)
+  // send back new access token (along with new refresh cookie)
   return res.json({ accessToken });
-  // } catch (err) {
-  // // remove token
-  // logger.info('refresh token expired');
-  // foundUser.refreshToken = [...newRefreshTokenArray];
-  // const result = await foundUser.save();
-  // logger.info(JSON.stringify(result));
-  // return res.status(403).json({ message: 'error' });
-  // }
 };
 
