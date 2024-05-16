645a79449845d57d9644de1eb76e2bdc108182a3
Adam Wozniak <adamwozhere@gmail.com>
2024-05-16 21:40:42 +0100

    change delete modal styling

--- client/src/components/ui/DeleteModal.tsx
+++ client/src/components/ui/DeleteModal.tsx
@@ -1,21 +1,15 @@
 import { useRef } from 'react';
 import Button from './Button';
+import ExclamationIcon from '../icons/ExclamationIcon';
 
 type Props = {
   open: boolean;
-  title: string;
   onConfirm: () => void;
   onClose: () => void;
   children: React.ReactNode;
 };
 
-export default function Modal({
-  open,
-  title,
-  onClose,
-  onConfirm,
-  children,
-}: Props) {
+export default function Modal({ open, onClose, onConfirm, children }: Props) {
   const dialogRef = useRef<null | HTMLDialogElement>(null);
 
   const closeDialog = () => {
@@ -33,11 +27,13 @@ export default function Modal({
   }
 
   return (
-    <dialog ref={dialogRef} className="rounded-xl">
-      <div className="bg-white p-8">
-        <h1 className="text-lg font-bold mb-4">{title}</h1>
+    <dialog ref={dialogRef} className="rounded-xl backdrop:bg-black/40">
+      <div className="flex flex-col items-center bg-white p-8 text-lg">
+        <div className="gap-2 items-center bg-red-200 text-red-500 font-bold p-2 text-sm mb-8 rounded-full border border-red-300">
+          <ExclamationIcon />
+        </div>
         {children}
-        <div className="flex gap-2 mt-8">
+        <div className="grid grid-cols-2 w-full gap-2 mt-8">
           <Button label="Cancel" variant="tertiary" onClick={closeDialog} />
           <Button label="Delete" variant="danger" onClick={confirmDialog} />
         </div>
